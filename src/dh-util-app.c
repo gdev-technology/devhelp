9057c5d50a9b194e27b3ce0562cca9448ad0fa98
Sébastien Wilmet <swilmet@gnome.org>
2026-03-01 19:13:45 +0100

    util-app: simplify after assistant removal
    dh_util_window_settings_save() and dh_util_window_settings_restore() are
    now always called with the same GSettings object which contains all the
    keys (for DhWindow).
    
    These functions were previously used also by DhAssistant, but it has
    been removed.

--- src/dh-util-app.c
+++ src/dh-util-app.c
@@ -1,59 +1,32 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
 /* SPDX-FileCopyrightText: 2001 Mikael Hallendal <micke@imendio.com>
  * SPDX-FileCopyrightText: 2004, 2008 Imendio AB
- * SPDX-FileCopyrightText: 2015, 2017, 2018 Sébastien Wilmet <swilmet@gnome.org>
+ * SPDX-FileCopyrightText: 2015-2026 Sébastien Wilmet <swilmet@gnome.org>
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #include "dh-util-app.h"
 
-static void
-introspect_window_gsettings (GSettings *window_settings,
-                             gboolean  *has_required_keys,
-                             gboolean  *has_maximized_key)
-{
-        GSettingsSchema *schema = NULL;
-
-        g_object_get (window_settings,
-                      "settings-schema", &schema,
-                      NULL);
-
-        *has_required_keys = (g_settings_schema_has_key (schema, "width") &&
-                              g_settings_schema_has_key (schema, "height"));
-
-        *has_maximized_key = g_settings_schema_has_key (schema, "maximized");
-
-        g_settings_schema_unref (schema);
-}
-
 void
 dh_util_window_settings_save (GtkWindow *window,
                               GSettings *settings)
 {
-        gboolean has_required_keys;
-        gboolean has_maximized_key;
+        GdkWindowState state;
+        gboolean maximized;
         gint width;
         gint height;
 
         g_return_if_fail (GTK_IS_WINDOW (window));
         g_return_if_fail (G_IS_SETTINGS (settings));
 
-        introspect_window_gsettings (settings, &has_required_keys, &has_maximized_key);
-        g_return_if_fail (has_required_keys);
+        state = gdk_window_get_state (gtk_widget_get_window (GTK_WIDGET (window)));
+        maximized = (state & GDK_WINDOW_STATE_MAXIMIZED) != 0;
 
-        if (has_maximized_key) {
-                GdkWindowState state;
-                gboolean maximized;
+        g_settings_set_boolean (settings, "maximized", maximized);
 
-                state = gdk_window_get_state (gtk_widget_get_window (GTK_WIDGET (window)));
-                maximized = (state & GDK_WINDOW_STATE_MAXIMIZED) != 0;
-
-                g_settings_set_boolean (settings, "maximized", maximized);
-
-                /* If maximized don't save the size. */
-                if (maximized)
-                        return;
-        }
+        /* If maximized don't save the size. */
+        if (maximized)
+                return;
 
         /* Store the dimensions */
         gtk_window_get_size (GTK_WINDOW (window), &width, &height);
@@ -69,17 +42,12 @@ void
 dh_util_window_settings_restore (GtkWindow *gtk_window,
                                  GSettings *settings)
 {
-        gboolean has_required_keys;
-        gboolean has_maximized_key;
         gint width;
         gint height;
 
         g_return_if_fail (GTK_IS_WINDOW (gtk_window));
         g_return_if_fail (gtk_widget_get_realized (GTK_WIDGET (gtk_window)));
         g_return_if_fail (G_IS_SETTINGS (settings));
-
-        introspect_window_gsettings (settings, &has_required_keys, &has_maximized_key);
-        g_return_if_fail (has_required_keys);
 
         width = g_settings_get_int (settings, "width");
         height = g_settings_get_int (settings, "height");
@@ -107,6 +75,6 @@ dh_util_window_settings_restore (GtkWindow *gtk_window,
                 gtk_window_set_default_size (gtk_window, width, height);
         }
 
-        if (has_maximized_key && g_settings_get_boolean (settings, "maximized"))
+        if (g_settings_get_boolean (settings, "maximized"))
                 gtk_window_maximize (gtk_window);
 }
