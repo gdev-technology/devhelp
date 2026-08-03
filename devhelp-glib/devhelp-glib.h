7d48e632bc0683bff92a500a92cbeb690daf2e49
Sébastien Wilmet <swilmet@gnome.org>
2026-08-03 13:41:27 +0200

    devhelp-glib: create new shared library for the backend only
    To separate the backend from the frontend.
    
    dh-completion.[ch] is a simple copy from the devhelp/ dir. A later
    commit will *use* devhelp-glib and remove dh-completion.[ch] from
    devhelp/.
    
    More and more code will be moved to devhelp-glib/ in later commits.

---
+++ devhelp-glib/devhelp-glib.h
@@ -0,0 +1,9 @@
+/* SPDX-FileCopyrightText: 2026 Sébastien Wilmet <swilmet@gnome.org>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+/* Include all the public headers. */
+
+#include <devhelp-glib/dh-completion.h>
